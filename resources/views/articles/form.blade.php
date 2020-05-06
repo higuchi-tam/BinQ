5dbf25f48f9332ac9a98c129d8f4b87cb7ad716d
脇田　慎平 <ymnkknt3@gmail.com>
2020-05-07 00:20:03 +0900

    記事編集　本文表示修正

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -7,11 +7,11 @@
 <div class="p-form__tag">
     <article-tags-input :initial-tags='@json($tagNames ?? [])' :autocomplete-items='@json($allTagNames ?? [])'
         class="p-form">
-        </article-ta$usergs-input>
+        </article-tags-input>
 </div>
 
 
 <div class="p-form__textarea">
-    <div class="js-quill-editor" data-target="#content" style="height: 150px;"></div>
-    <input id="content" name="body" type="hidden" value="{{ $article->title ?? old('body') }}">
+    <div class="js-quill-editor" data-target="#content" style="height: 150px;">{!! $article->body ?? old('body') !!}</div>
+    <input id="content" name="body" type="hidden" value="">
 </div>
