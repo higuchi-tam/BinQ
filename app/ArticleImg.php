b90bdc5cfb70c0769474efdb850a130cb3489a65
脇田　慎平 <ymnkknt3@gmail.com>
2020-05-07 00:22:46 +0900

    記事内画像 モデル追加

---
+++ app/ArticleImg.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class ArticleImg extends Model
+{
+    public function articles()
+    {
+        return $this->belongsToMany('App\Article');
+    }
+}
