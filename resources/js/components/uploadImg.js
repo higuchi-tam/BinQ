25daa55bdf41bbdb4cf4a6cb94ae077cd32b59d4
脇田　慎平 <ymnkknt3@gmail.com>
2020-05-13 23:24:10 +0900

    画像の非同期追加jsリファクタ

--- resources/js/components/uploadImg.js
+++ resources/js/components/uploadImg.js
@@ -1,20 +1,23 @@
 import jquery from "jquery";
 window.$ = jquery;
 
-//記事本文の画像投稿
-$(document).on('change','#js-upload__img', function() {
-    let $tgt = $(this);
-    let file = this.files[0];
-    let formData = new FormData();
+$(function () {
+    
     let articleId = $('#js-articleId__for-ajax').attr('data-article__id');
-    let postType = "body";
-
-   
-    formData.append('file', file);
-    formData.append('article_id', articleId);
-    formData.append('postType', postType);
-
-
+    const $prevArea = $('#js-img__prevArea');
+    const $delete = $('#js-img__delete')
+    //==========================================================
+    //記事本文の画像投稿
+    //==========================================================≈
+    $(document).on('change','#js-upload__img', function() {
+        let file = this.files[0];
+        let formData = new FormData();
+        let postType = "body";
+    
+        formData.append('file', file);
+        formData.append('article_id', articleId);
+        formData.append('postType', postType);
+    
     $.ajax({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -29,118 +32,117 @@ $(document).on('change','#js-upload__img', function() {
         // Ajaxリクエストが成功した場合
         .done(function (data) {
             //現在のカーソル位置を特定し、そこに画像を挿入する
-            // let $editor = window.editor;
             let cursor = $('#js-articleId__for-ajax').attr('data-cursor__index');
             let newPosition = cursor ? cursor : editor.getLength();
-            console.log('newPosition');
-            console.log(newPosition);
-            console.log('newPosition+1');
-            console.log(Number(newPosition) + 1);
-
-            console.log('ここまで1-2');
             editor.setSelection(newPosition,0);
-
-            console.log('editor.getSelection()');
-            console.log(editor.getSelection());
-
+            
             editor.updateContents(
                 new Delta()
-                    .retain(editor.getSelection().index)
+                .retain(editor.getSelection().index)
+                    .insert('\n')
                     .insert({
                         image: '/storage/'+data
                     })
-            );
-            editor.setSelection(Number(newPosition) + 1,0);
-        })
-        // Ajaxリクエストが失敗した場合
-        .fail(function (data) {
-            let status = data.status;
+                    .insert('\n')
+                    );
+                    editor.setSelection(Number(newPosition) + 1,0);
+                })
+                // Ajaxリクエストが失敗した場合
+                .fail(function (data) {
+                    let status = data.status;
             let errMessage;
 
             switch (status) {
                 case 413:
                     errMessage = 'アップロード上限は2MBです';
                     break;
-                case 422:
-                    errMessage = 'アップロードできるのは画像のみです';
+                    case 422:
+                        errMessage = 'アップロードできるのは画像のみです';
                     break;
-                default:
-                    errMessage = 'アップロードに失敗しました。もう一度やり直してください';
-            }
+                    default:
+                        errMessage = 'アップロードに失敗しました。もう一度やり直してください';
+                    }
             alert(errMessage);
         })
-})
-
-//記事のヘッダー画像登録
-$(document).on('change','#js-upload-mainImg', function() {
-    let $tgt = $(this);
-    let file = this.files[0];
-    let formData = new FormData();
-    let articleId = $('#js-articleId__for-ajax').attr('data-article__id');
-    let postType = "header";
-   
-    formData.append('file', file);
-    formData.append('article_id', articleId);
-    formData.append('postType', postType);
-
-    $.ajax({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        url: '/articles/ajaxImgUpload',
-        type: 'POST',
-        dataType: 'json',
-        processData: false,
-        contentType: false,
-        data: formData,
     })
+    //==========================================================
+    //記事のヘッダー画像登録
+    //==========================================================
+    $(document).on('change','#js-upload-mainImg', function() {
+        let file = this.files[0];
+        let formData = new FormData();
+        let postType = "header";
+    
+        formData.append('file', file);
+        formData.append('article_id', articleId);
+        formData.append('postType', postType);
+        
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            url: '/articles/ajaxImgUpload',
+            type: 'POST',
+            dataType: 'json',
+            processData: false,
+            contentType: false,
+            data: formData,
+        })
         // Ajaxリクエストが成功した場合
         .done(function (data) {
-            $('#js-img__prevArea').css('background-image', 'url("/storage/'+ data + '")');
+            $prevArea.css({
+                'background-image': 'url("/storage/' + data + '")',
+            });
+            $prevArea.addClass('hasImg');
+            $delete.addClass('hasImg');
         })
         // Ajaxリクエストが失敗した場合
         .fail(function (data) {
             let status = data.status;
             let errMessage;
-
+            
             switch (status) {
                 case 413:
                     errMessage = 'アップロード上限は2MBです';
                     break;
                 case 422:
-                    errMessage = 'アップロードできるのは画像のみです';
+                        errMessage = 'アップロードできるのは画像のみです';
                     break;
                 default:
-                    errMessage = 'アップロードに失敗しました。もう一度やり直してください';
-            }
+                        errMessage = 'アップロードに失敗しました。もう一度やり直してください';
+                }
             alert(errMessage);
+            })
         })
-})
-
-//記事のヘッダー画像削除
-$(document).on('click', '#js-img__delete', function () {
-    let articleId = $('#js-articleId__for-ajax').attr('data-article__id');
-    let isDelete = 1;
-    let postType = "header";
-
-    let postData = {
-        "article_id" : articleId,
-        "postType":postType,
-        "isDelete": isDelete,
-    };
-
-    $.ajax({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        url: '/articles/ajaxImgUpload',
-        type: 'POST',
-        dataType: 'json',
-        data: postData,
-    })
+    //==========================================================
+    //記事のヘッダー画像削除
+    //==========================================================
+    $(document).on('click', '#js-img__delete', function () {
+        let isDelete = 1;
+        let postType = "header";
+        
+        let postData = {
+            "article_id" : articleId,
+            "postType":postType,
+            "isDelete": isDelete,
+        };
+        
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            url: '/articles/ajaxImgUpload',
+            type: 'POST',
+            dataType: 'json',
+            data: postData,
+        })
         // Ajaxリクエストが成功した場合
         .done(function (data) {
-            $('#js-img__prevArea').css('background-image', '');
+            $prevArea.css({
+                'background-image': 'url("/images/image-plus.svg")',
+            });
+            $prevArea.removeClass('hasImg');
+            $delete.removeClass('hasImg');
         })
         // Ajaxリクエストが失敗した場合
         .fail(function (data) {
@@ -156,7 +158,8 @@ $(document).on('click', '#js-img__delete', function () {
                     break;
                 default:
                     errMessage = 'アップロードに失敗しました。もう一度やり直してください';
-            }
-            alert(errMessage);
-        })
-});
+                }
+                alert(errMessage);
+            })
+    });
+})
