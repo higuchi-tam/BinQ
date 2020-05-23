99f84a1121be5d9185957e418f4d3d92ec9d860e
脇田　慎平 <ymnkknt3@gmail.com>
2020-05-24 01:09:17 +0900

    サイドバー見た目

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,9 +3,9 @@
         <h4 class="c-category__title">人気の美容師</h4>
         <ul>
             @foreach($users as $user)
-            　　@if ($loop->index < 5) @include('users.user_side')@endif @endforeach </ul> <div class="c-button__sns">
-                <a href="/users" class="c-button__sidebar">人気の美容師一覧</a>
-    </div>
+            　　@if ($loop->index < 5) @include('users.user_side')@endif @endforeach </ul> <a href="/users"
+                class="c-button__sidebar">人気の美容師一覧</a>
+
     </div>
 
     <div class="c-article">
@@ -13,9 +13,9 @@
         <h4 class="c-category__title">人気記事</h4>
         <ul>
             @foreach($articles as $article)
-            　　@if ($loop->index < 5) @include('articles.card_side')@endif @endforeach </ul> <div class="c-button__sns">
-                <a href="/likeIndex" class="c-button__sidebar">人気記事一覧</a>
-    </div>
+            　　@if ($loop->index < 5) @include('articles.card_side')@endif @endforeach </ul> <a href="/likeIndex"
+                class="c-button__sidebar">人気記事一覧</a>
+
     </div>
 
 </section>
