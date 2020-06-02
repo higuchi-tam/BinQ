afb74292bde3c54ed6490930493a17e010bb6680
脇田　慎平 <ymnkknt3@gmail.com>
2020-06-03 00:14:05 +0900

    ogp設定

--- resources/views/layouts/meta_sns.blade.php
+++ resources/views/layouts/meta_sns.blade.php
@@ -5,5 +5,9 @@
 <meta property="og:site_name" content="BinQ" />
 <meta property="og:image" content="{{asset('/storage/'.$article->img)}}" />
 
+<meta name="twitter:creator" content="@kiwatchi1991" />
+<meta name="twitter:title" content="【記事タイトル７０文字まで】" />
+<meta name="twitter:description" content="【記事概要２００文字まで】" />
+
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:site" content="@nishimachikid" />
