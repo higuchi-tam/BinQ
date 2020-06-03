8ec5d4f55586b050ea6f0e186870360f1d8d0263
脇田　慎平 <ymnkknt3@gmail.com>
2020-06-04 01:24:06 +0900

    ogp設定コンポーネント化

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,15 +6,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{-- <meta http-equiv="content-type" content="text/html; charset=utf-8"> --}}
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:site" content="@kiwatchi1991" />
-    <meta property="og:url" content="https://binq.kiwatchi.com/" />
-    <meta property="og:title" content="サルワカの殿堂入り記事集" />
-    <meta property="og:description" content="これまで公開してきた200以上の記事の中で、特に人気のあったものを紹介します。" />
-    <meta property="og:site_name" content="BinQ" />
-    <meta property="og:image" content="https://binq.kiwatchi.com/images/twitter_card.png" />
-    {{-- @yield('meta_sns') --}}
+
+    @yield('meta_sns')
     <!--CSS -->
     <!-- Styles -->
     {{-- TODO:本番環境ではsecure_assetに切り替える --}}
--- resources/views/layouts/meta_sns.blade.php
+++ resources/views/layouts/meta_sns.blade.php
@@ -1,20 +1,7 @@
-{{-- <meta property="og:url" content="{{ route('articles.show',$article->id) }}" />
-<meta property="og:type" content="article" />
-<meta property="og:title" content="{{ $article->title }}" />
-<meta property="og:description" content="{{ $article->body }}" />
-<meta property="og:site_name" content="BinQ" />
-<meta property="og:image" content="{{asset('/storage/'.$article->img)}}" />
-
-<meta name="twitter:creator" content="@kiwatchi1991" />
-<meta name="twitter:title" content="【記事タイトル７０文字まで】" />
-<meta name="twitter:description" content="【記事概要２００文字まで】" />
-
-<meta name="twitter:card" content="summary_large_image" />
-<meta name="twitter:site" content="@kiwatchi1991" /> --}}
-
-{{-- <meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:card" content="summary">
 <meta name="twitter:site" content="@kiwatchi1991" />
-<meta property="og:url" content="https://binq.kiwatchi.com/articles/91" />
+<meta property="og:url" content="https://binq.kiwatchi.com/" />
 <meta property="og:title" content="サルワカの殿堂入り記事集" />
 <meta property="og:description" content="これまで公開してきた200以上の記事の中で、特に人気のあったものを紹介します。" />
-<meta property="og:image" content="https://binq.kiwatchi.com/images/blank_profile.png" /> --}}
+<meta property="og:site_name" content="BinQ" />
+<meta property="og:image" content="https://binq.kiwatchi.com/images/twitter_card.png" />
