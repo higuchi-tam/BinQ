5a7560764fe4042d9971cd6a79dccb95fab52740
脇田　慎平 <ymnkknt3@gmail.com>
2020-06-03 23:36:53 +0900

    ogp設定

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <meta property="og:url" content="https://binq.kiwatchi.com/articles/91" />
     <meta property="og:title" content="サルワカの殿堂入り記事集" />
     <meta property="og:description" content="これまで公開してきた200以上の記事の中で、特に人気のあったものを紹介します。" />
+    <meta property="og:site_name" content="BinQ" />
     <meta property="og:image" content="https://binq.kiwatchi.com/images/blank_profile.png" />
     {{-- @yield('meta_sns') --}}
     <!--CSS -->
