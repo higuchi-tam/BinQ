dfd76d8765aee0c24107ff4e39ac17c19e9c5b24
脇田　慎平 <ymnkknt3@gmail.com>
2020-06-06 14:00:27 +0900

    fb設定

--- resources/views/layouts/meta_sns.blade.php
+++ resources/views/layouts/meta_sns.blade.php
@@ -1,5 +1,3 @@
-<meta name="twitter:card" content="summary">
-<meta name="twitter:site" content="@nichimachikid" />
 @if($type === "article")
 <meta property="og:url" content="{{ route('articles.show',$article->id) }}" />
 <meta property="og:title" content="{{ $article->title }}" />
@@ -9,5 +7,11 @@
 <meta property="og:title" content="美容師で、つながる。" />
 <meta property="og:description" content="新しい、美容師のSNS" />
 @endif
+
 <meta property="og:site_name" content="BinQ" />
 <meta property="og:image" content="https://binq.kiwatchi.com/images/twitter_card2.png" />
+{{-- twitter設定 --}}
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@nichimachikid" />
+{{-- facebook設定 --}}
+<meta property="fb:app_id" content="257381682124442" />
